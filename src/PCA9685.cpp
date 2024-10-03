95c082c18030218c93b15640d681e2ab29299611
NebBackwards <benproctor2022@gmail.com>
2024-10-03 11:23:29 -0400

    more work on PCA9685, added servoWriteMicros implementation & setFrequency implementation

--- src/PCA9685.cpp
+++ src/PCA9685.cpp
@@ -7,14 +7,17 @@
 PCA9685::PCA9685(uint8_t address)
 {
     i2c_address = address;
+    oscClock = 25000000; //25MHz internal oscillator clock
     Wire.begin();
+    setFrequency(200);
     return;
 }
 
-void PCA9685::setClkFreq(int freq, int pin){
+void PCA9685::setFrequency(int freq){ //set frequency of control signal in Hz
 
-    Wire.beginTransmission(i2c_address);
-    Wire.write(8);
+    int prescale_value = round(oscClock/(4096*freq)) - 1;
+    writeReg(PRE_SCALE, prescale_value);
+    frequency = freq;
     return;
 }
 
@@ -26,8 +29,31 @@ void PCA9685::writeReg(uint8_t reg, uint8_t value)
     last_status = Wire.endTransmission();
 }
 
-uint8_t PCA9685::getServoReg(uint8_t servoNum){ //returns register address for 
+uint8_t PCA9685::getServoReg(int servoNum){ //returns register address of SERVOn_ON_L
 
-    return 6 + servoNum*4;
+    return SERVO0_ON_L + servoNum*4; //4 addresses per servo
 
+}
+
+void PCA9685::servoWriteMicros(int micros, int servoNum)
+{
+    float microsPerCycle = (1000000/frequency);
+    float microsPerBit = microsPerCycle/4096;
+    int bitsOn = micros/(int)microsPerBit;
+    uint16_t onTime = 500; //arbitrary
+    uint16_t offTime = onTime + bitsOn;
+    uint8_t writeVals[4];
+        writeVals[0] = (onTime & 0xF00)>>8; //SERVOn_ON_H
+        writeVals[1] = onTime & 0xFF; //SERVOn_ON_L
+        writeVals[2] = (offTime & 0xF00)>>8; //SERVOn_OFF_H
+        writeVals[3] = offTime & 0xFF; //SERVOn_OFF_L
+    int SERVOn_ON_L = getServoReg(servoNum);
+    for(int i=0;i<4;i++){
+        writeReg(SERVOn_ON_L+i, writeVals[i]);
+    }
+    return;
+}
+
+void PCA9685::setAngle(float angle, int servoNum){
+    
 }
--- src/PCA9685.h
+++ src/PCA9685.h
@@ -1,17 +1,21 @@
 #include <stdint.h>
 #include <Wire.h>
 #include <stdio.h>
+#define PRE_SCALE 0xFE
+#define SERVO0_ON_L 0x06
 
 class PCA9685
 {
 private:
     uint8_t i2c_address;
     uint8_t last_status;
+    int oscClock;
+    int frequency;
 public:
     PCA9685(uint8_t address);
-    void servoSetFreq(int freq, int pin);
+    void setFrequency(int freq);
     void writeReg(uint8_t reg, uint8_t value);
-    uint8_t getServoReg(uint8_t servoNum);
-    void servoWriteMicros(int mircos, int servoNum);
+    uint8_t getServoReg(int servoNum);
+    void servoWriteMicros(int micros, int servoNum);
     void setAngle(float angle, int servoNum);
 };
